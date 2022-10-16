7648b8f02865468732824d3f79f1e28dcf14d556
iyabc <juliennepanes@gmail.com>
2022-10-16 22:51:37 +0800

    minor changes

--- renderer/src/components/EmployeeManagementSystem/Attendance/AttendanceForm/AttendanceForm.jsx
+++ renderer/src/components/EmployeeManagementSystem/Attendance/AttendanceForm/AttendanceForm.jsx
@@ -1,10 +1,10 @@
-import { Modal, Box } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import AddAttendanceModal from '../AddAttendanceModal/AddAttendanceModal';
-import BigButton from '../../Shared/Buttons/BigButton/BigButton';
-import styles from './AttendanceForm.module.scss';
+import { Modal, Box } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import AddAttendanceModal from "../AddAttendanceModal/AddAttendanceModal";
+import BigButton from "../../Shared/Buttons/BigButton/BigButton";
+import styles from "./AttendanceForm.module.scss";
 import Rest from "../../../../rest/Rest.tsx";
-import { useUser } from '../../../contexts/UserContext';
+import { useUser } from "../../../contexts/UserContext";
 
 const INITIAL_URL = process.env.NEXT_PUBLIC_INITIAL_URL;
 
@@ -15,91 +15,89 @@ function AttendanceForm() {
   const [attendance, setAttendance] = useState([]);
   const handleAttendanceData = (data) => {
     setAttendance(data);
-  }
+  };
   const getAttendanceData = () => {
-    rest.get(`${INITIAL_URL}/attendance/${employeeName}`, handleAttendanceData)
-  }
-  //get recent attendance
+    rest.get(`${INITIAL_URL}/attendance/${employeeName}`, handleAttendanceData);
+  };
+  const [recentAttendance, setRecentAttendance] = useState();
   const findRecentAttendance = () => {
-    if(attendance.length > 0){
-      let recentDate = null;
-      attendance.map((item) => {
-        if(recentDate == null){
-          recentDate = item.attendanceTime;
-        }
-        else{
-          if(recentDate > item.attendanceTime){
-            recentDate = item.attendanceTime;
-          }else {
-            recentDate = item.attendanceTime
-          }
-        }
-      })
-      let type = null;
-      attendance.map((item) => {
-        if(recentDate == item.attendanceTime){
-          type = item.attendanceType;
-        }
-      })
-      let output = `Recent Attendance: ${recentDate} | ${type}`
-      return output;
-    }else{
-      return `Recent Attendance: No Attendance History.`
+    if (attendance.length > 0) {
+      const arr = attendance.sort(
+        (a, b) => new Date(b.attendanceTime) - new Date(a.attendanceTime)
+      );
+      setRecentAttendance(
+        `${arr[0].attendanceTime} | ${arr[0].attendanceType}`
+      );
+    } else {
+      setRecentAttendance("None");
     }
-  }
+  };
   //confirm add attendance modal
   const [openAddAttendanceModal, setOpenAddAttendanceModal] = useState(false);
-  const handleOpenAddAttendanceModal = () => { setOpenAddAttendanceModal(true) };
-  const handleCloseAddAttendanceModal = () => { setOpenAddAttendanceModal(false) };
+  const handleOpenAddAttendanceModal = () => {
+    setOpenAddAttendanceModal(true);
+  };
+  const handleCloseAddAttendanceModal = () => {
+    setOpenAddAttendanceModal(false);
+  };
   //
-  const [datetime, setDatetime] = useState('');
-  const [attendanceType, setAttendanceType] = useState('')
+  const [datetime, setDatetime] = useState("");
+  const [attendanceType, setAttendanceType] = useState("");
   //post check in
   const postCheckIn = () => {
     setDatetime(new Date());
-    setAttendanceType('CHECK_IN');
+    setAttendanceType("CHECK_IN");
     handleOpenAddAttendanceModal();
-  }
+  };
   //post check out
   const postCheckOut = () => {
     setDatetime(new Date());
-    setAttendanceType('CHECK_OUT');
+    setAttendanceType("CHECK_OUT");
     handleOpenAddAttendanceModal();
-  }
+  };
   const addSuccessAction = () => {
+    getAttendanceData();
     handleCloseAddAttendanceModal();
-  }
+  };
 
   useEffect(() => {
     getAttendanceData();
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    findRecentAttendance();
+  }, [attendance]);
 
   return (
     <div className={styles.container}>
       <div className={styles.header}>ATTENDANCE</div>
       <div className={styles.form}>
         <div className={styles.row}>
-          <div className={styles.btn_container} onClick={postCheckIn}><BigButton label="IN" link=""/></div>
-          <div className={styles.btn_container} onClick={postCheckOut}><BigButton label="OUT" link=""/></div>
+          <div className={styles.btn_container} onClick={postCheckIn}>
+            <BigButton label="IN" link="" />
+          </div>
+          <div className={styles.btn_container} onClick={postCheckOut}>
+            <BigButton label="OUT" link="" />
+          </div>
         </div>
-        <div className={styles.row}>
-          {findRecentAttendance()}
-        </div>
+        <div className={styles.row}>Recent Attendance: {recentAttendance}</div>
       </div>
 
-      {/* confirm add attendance modal */}
-      <Modal open={openAddAttendanceModal} onClose={handleCloseAddAttendanceModal}>
-          <Box className={styles.modal}>
-            <AddAttendanceModal 
-              addSuccessAction={addSuccessAction}
-              employeeName={employeeName} 
-              attendanceType={attendanceType}
-              attendanceTime={datetime}
-            />
-          </Box>
-        </Modal>
+      <Modal
+        open={openAddAttendanceModal}
+        onClose={handleCloseAddAttendanceModal}
+      >
+        <Box className={styles.modal}>
+          <AddAttendanceModal
+            addSuccessAction={addSuccessAction}
+            employeeName={employeeName}
+            attendanceType={attendanceType}
+            attendanceTime={datetime}
+          />
+        </Box>
+      </Modal>
     </div>
-  )
+  );
 }
 
-export default AttendanceForm
+export default AttendanceForm;
